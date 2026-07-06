8a73f97bebb2e98d848d9e558d610510de296141
cyrox007 <cyrox007@gmail.com>
2026-07-06 15:15:07 +0300

    умбла имя сайта в env

--- apps/frontend/src/constants/seo.ts
+++ apps/frontend/src/constants/seo.ts
@@ -1,5 +1,5 @@
 export const TITLE_SEPARATOR = ' – ';
-export const SITE_NAME = 'Светофор Мебели';
+export const SITE_NAME = import.meta.env.VITE_API_SITENAME;
 
 export const buildTitle = (page: string): string => {
 	return `${page}${TITLE_SEPARATOR}${SITE_NAME}`;
