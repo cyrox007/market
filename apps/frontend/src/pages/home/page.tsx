3f1a1d782921006eef062ffc7b7276a5830fd6b7
cyrox007 <cyrox007@gmail.com>
2026-07-06 13:25:25 +0300

    мета для главгной

--- apps/frontend/src/pages/home/page.tsx
+++ apps/frontend/src/pages/home/page.tsx
@@ -5,7 +5,19 @@ import InteriorIdeas from './components/InteriorIdeas';
 import WhyChooseUs from './components/WhyChooseUs';
 import Newsletter from './components/Newsletter';
 
+import { usePageSeo } from '../../hooks/usePageSeo';
+
 export default function Home() {
+	usePageSeo({
+		title: 'Главная | Светофор-Мебель',
+		description: 'Интернет-магазин мебели Светофор-Мебель',
+		image: '/logo.png',
+		canonical_url: window.location.href,
+		robots: 'index, follow',
+		open_graph_title: 'Главная | Светофор-Мебель',
+		locale: 'ru_RU',
+	});
+	
 	return (
 		<div className="min-h-screen bg-white">
 			<Hero />
