9b264af7aa7c40f84a9cd61ec0713f6cabd0fe9f
cyrox007 <cyrox007@gmail.com>
2026-08-04 10:36:27 +0300

    f

--- services/backend/app/Jobs/ProcessProductImages.php
+++ services/backend/app/Jobs/ProcessProductImages.php
@@ -10,7 +10,6 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
-use Spatie\MediaLibrary\MediaCollections\Exceptions\FileCannotBeAdded;
 
 class ProcessProductImages implements ShouldQueue
 {
