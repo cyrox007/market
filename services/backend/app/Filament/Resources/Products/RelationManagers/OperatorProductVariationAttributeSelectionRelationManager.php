d657ef27803868391a1bf1f73268bc26d3de56a5
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-14 11:51:32 +0300

    Fix variation parameters override signature

--- services/backend/app/Filament/Resources/Products/RelationManagers/OperatorProductVariationAttributeSelectionRelationManager.php
+++ services/backend/app/Filament/Resources/Products/RelationManagers/OperatorProductVariationAttributeSelectionRelationManager.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\Products\RelationManagers;
 
 use Illuminate\Auth\Access\AuthorizationException;
-use Illuminate\Database\Eloquent\Model;
 
 use function Filament\authorize;
 
@@ -11,7 +10,7 @@ class OperatorProductVariationAttributeSelectionRelationManager extends ProductV
 {
     protected static ?string $title = 'Параметры вариаций';
 
-    public static function canViewForRecord(Model $ownerRecord, string $pageClass): bool
+    public static function canViewForRecord($ownerRecord, string $pageClass): bool
     {
         if ($ownerRecord->isVariant()) {
             return false;
