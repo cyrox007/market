894361799d4c7c2b95dd59ce1c686de669f19cd6
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-14 16:22:47 +0300

    ui(admin): add product editor section navigation

---
+++ services/backend/resources/views/filament/resources/products/components/editor-section-nav.blade.php
@@ -0,0 +1,52 @@
+<div class="sticky top-4 z-20">
+    <x-filament::tabs label="Разделы товара">
+        <x-filament::tabs.item
+            tag="a"
+            href="#product-main"
+            icon="heroicon-m-information-circle"
+            active
+        >
+            Основное
+        </x-filament::tabs.item>
+
+        <x-filament::tabs.item
+            tag="a"
+            href="#product-description"
+            icon="heroicon-m-document-text"
+        >
+            Описание
+        </x-filament::tabs.item>
+
+        <x-filament::tabs.item
+            tag="a"
+            href="#product-attributes"
+            icon="heroicon-m-adjustments-horizontal"
+        >
+            Характеристики
+        </x-filament::tabs.item>
+
+        <x-filament::tabs.item
+            tag="a"
+            href="#product-stock"
+            icon="heroicon-m-archive-box"
+        >
+            Остатки
+        </x-filament::tabs.item>
+
+        <x-filament::tabs.item
+            tag="a"
+            href="#product-media"
+            icon="heroicon-m-photo"
+        >
+            Медиа
+        </x-filament::tabs.item>
+
+        <x-filament::tabs.item
+            tag="a"
+            href="#product-extra"
+            icon="heroicon-m-cog-6-tooth"
+        >
+            Дополнительно
+        </x-filament::tabs.item>
+    </x-filament::tabs>
+</div>
