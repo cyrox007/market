8c9edf5c874efb08fa67c78b637605415cf97f1a
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-14 10:49:35 +0300

    ui(admin): add save and exit action to room edit

--- services/backend/app/Filament/Resources/Rooms/Pages/EditRoom.php
+++ services/backend/app/Filament/Resources/Rooms/Pages/EditRoom.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\Rooms\Pages;
 
 use App\Filament\Resources\Rooms\RoomResource;
 use App\Filament\Resources\Rooms\Schemas\RoomForm;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
@@ -11,11 +12,46 @@ class EditRoom extends EditRecord
 {
     protected static string $resource = RoomResource::class;
 
+    public static bool $formActionsAreSticky = true;
+
+    protected bool $exitAfterSave = false;
+
     protected function getHeaderActions(): array
     {
         return [
             DeleteAction::make(),
         ];
+    }
+
+    protected function getFormActions(): array
+    {
+        return [
+            $this->getSaveFormAction()
+                ->label('Сохранить')
+                ->icon('heroicon-m-check'),
+            Action::make('saveAndExit')
+                ->label('Сохранить и выйти')
+                ->icon('heroicon-m-arrow-right-start-on-rectangle')
+                ->color('gray')
+                ->action('saveAndExit')
+                ->keyBindings(['mod+shift+s']),
+            $this->getCancelFormAction(),
+        ];
+    }
+
+    public function saveAndExit(): void
+    {
+        $this->exitAfterSave = true;
+        $this->save();
+    }
+
+    protected function getRedirectUrl(): ?string
+    {
+        if ($this->exitAfterSave) {
+            return static::getResource()::getUrl('index');
+        }
+
+        return parent::getRedirectUrl();
     }
 
     protected function mutateFormDataBeforeFill(array $data): array
